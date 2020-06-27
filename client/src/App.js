db3e6a6db5bc6ca80162d5e2fbaa6e76aa57fa57
= <sandrock11@hotmail.com>
2020-06-26 23:17:36 -0400

    check local storage for token to load user

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,20 @@ import store from "./store";
 import { Provider } from "react-redux";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import { loadUser } from "./actions/authActions";
 
 import "./App.css";
+import setAuthToken from "./util/setAuthToken";
 
 const App = () => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
   //initializes materialize javascript
   useEffect(() => {
     M.AutoInit();
-  });
+    store.dispatch(loadUser());
+  }, []);
 
   return (
     <Router>
