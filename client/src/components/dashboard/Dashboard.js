35c392955ae891af1e7c2f5054a4504ee493d35c
= <sandrock11@hotmail.com>
2020-06-28 21:35:39 -0400

    profile and auth information is sent to dashboard

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profileActions";
+
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.authReducer,
+  profile: state.profileReducer,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
