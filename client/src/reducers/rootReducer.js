1c931353f1853404b1960683e0b03b7b360e69c2
= <sandrock11@hotmail.com>
2020-06-28 21:37:40 -0400

    added profileReducer to root reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "../reducers/authReducer";
+import profileReducer from "../reducers/profileReducer";
 
 export default combineReducers({
   authReducer,
+  profileReducer,
 });
