c5af763089662f7f8de50ba6d3fa346af25a502b
= <sandrock11@hotmail.com>
2020-06-28 21:36:07 -0400

    navbar links divided into guest and auth

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/authActions";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  //create variabes that will store links visible to the guests and members
+  const authLinks = (
+    <ul id="nav-mobile" className="right hide-on-med-and-down">
+      <li>
+        <a
+          onClick={logout}
+          href="#!"
+          to="/enthusiasts"
+          className="tooltipped"
+          data-position="bottom"
+          data-tooltip="Meet Manga Lovers"
+        >
+          <i className="material-icons left landing-btn">logout</i>
+          Logout
+        </a>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul id="nav-mobile" className="right hide-on-med-and-down">
+      <li>
+        <Link
+          to="/login"
+          className="tooltipped"
+          data-position="bottom"
+          data-tooltip="Log in to your account"
+        >
+          <i className="material-icons left landing-btn">login</i>
+          Login
+        </Link>
+      </li>
+      <li>
+        <Link
+          to="/register"
+          className="tooltipped"
+          data-position="bottom"
+          data-tooltip="Create an account"
+        >
+          <i className="material-icons left landing-btn">keyboard_hide</i>
+          Register
+        </Link>
+      </li>
+      <li>
+        <Link
+          to="/enthusiasts"
+          className="tooltipped"
+          data-position="bottom"
+          data-tooltip="Meet Manga Lovers"
+        >
+          <i className="material-icons left landing-btn">people</i>
+          Enthusiasts
+        </Link>
+      </li>
+    </ul>
+  );
   return (
     <div className="navbar-fixed">
       <nav className="grey darken-4">
@@ -11,49 +70,24 @@ const Navbar = () => {
               <i className="large material-icons">tv</i>
               <span className="text-primary">Anime</span> Enthusiasts
             </Link>
-
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li>
-                <Link
-                  to="/login"
-                  className="tooltipped"
-                  data-position="bottom"
-                  data-tooltip="Log in to your account"
-                >
-                  <i className="material-icons left landing-btn">login</i>
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/register"
-                  className="tooltipped"
-                  data-position="bottom"
-                  data-tooltip="Create an account"
-                >
-                  <i className="material-icons left landing-btn">
-                    keyboard_hide
-                  </i>
-                  Register
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/enthusiasts"
-                  className="tooltipped"
-                  data-position="bottom"
-                  data-tooltip="Meet Manga Lovers"
-                >
-                  <i className="material-icons left landing-btn">people</i>
-                  Enthusiasts
-                </Link>
-              </li>
-            </ul>
           </div>
         </div>
+
+        {/* if not loading and authenticated show authlinks else guest links */}
+        {!loading && (
+          <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+        )}
       </nav>
     </div>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  auth: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.authReducer,
+});
+export default connect(mapStateToProps, { logout })(Navbar);
