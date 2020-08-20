b1193a9eb95cd280ca3f8a83b94d3ac1d2253503
= <sandrock11@hotmail.com>
2020-08-20 13:27:01 -0400

    materialize narvbar styling edits

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,6 +9,17 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul id="nav-mobile" className="right hide-on-med-and-down">
       <li>
+        <Link
+          to="/dashboard"
+          className="tooltipped"
+          data-position="bottom"
+          data-tooltip="View Dashboard"
+        >
+          <i className="material-icons left landing-btn">account_box</i>
+          Dashboard
+        </Link>
+      </li>
+      <li>
         <a
           onClick={logout}
           href="#!"
@@ -61,23 +72,100 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
       </li>
     </ul>
   );
-  return (
-    <div className="navbar-fixed">
-      <nav className="grey darken-4">
-        <div className="container">
-          <div className="nav-wrapper">
-            <Link to="/" className="brand-logo">
-              <i className="large material-icons">tv</i>
-              <span className="text-primary">Anime</span> Enthusiasts
-            </Link>
-          </div>
-        </div>
 
-        {/* if not loading and authenticated show authlinks else guest links */}
-        {!loading && (
-          <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
-        )}
-      </nav>
+  const mobileGuest = (
+    <ul class="sidenav black" id="mobile-demo">
+      <li>
+        <Link
+          to="/login"
+          className="tooltipped"
+          data-position="bottom"
+          data-tooltip="Log in to your account"
+        >
+          <i className="white-text material-icons left landing-btn">login</i>
+          Login
+        </Link>
+      </li>
+      <li>
+        <Link
+          to="/register"
+          className="tooltipped"
+          data-position="bottom"
+          data-tooltip="Create an account"
+        >
+          <i className="white-text material-icons left landing-btn">
+            keyboard_hide
+          </i>
+          Register
+        </Link>
+      </li>
+      <li>
+        <Link
+          to="/enthusiasts"
+          className="tooltipped"
+          data-position="bottom"
+          data-tooltip="Meet Manga Lovers"
+        >
+          <i className=" white-text material-icons left landing-btn">people</i>
+          Enthusiasts
+        </Link>
+      </li>
+    </ul>
+  );
+
+  const mobileAuth = (
+    <ul class="sidenav black text-white" id="mobile-demo">
+      <li>
+        <Link
+          to="/dashboard"
+          className="tooltipped"
+          data-position="bottom"
+          data-tooltip="View Dashboard"
+        >
+          <i className="material-icons left landing-btn">account_box</i>
+          Dashboard
+        </Link>
+      </li>
+      <li>
+        <a
+          onClick={logout}
+          href="#!"
+          to="/enthusiasts"
+          className="tooltipped"
+          data-position="bottom"
+          data-tooltip="Meet Manga Lovers"
+        >
+          <i className="material-icons left landing-btn">logout</i>
+          Logout
+        </a>
+      </li>
+    </ul>
+  );
+  return (
+    <div>
+      <div className="navbar-fixed">
+        <nav className="grey  darken-4 navbar-fixed">
+          <div className="container">
+            <div className="nav-wrapper">
+              <Link to="/" className="brand-logo">
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+                  <i class="material-icons">menu</i>
+                </a>
+                <i className="large material-icons">tv</i>
+                <span className="text-primary">Anime</span>{" "}
+                <span className="small">Enthusiasts</span>
+              </Link>
+            </div>
+          </div>
+
+          {/* if not loading and authenticated show authlinks else guest links */}
+          {!loading && (
+            <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+          )}
+        </nav>
+      </div>
+
+      {!loading && isAuthenticated ? mobileAuth : mobileGuest}
     </div>
   );
 };
