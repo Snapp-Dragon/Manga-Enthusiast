929c993ec8c647af992987650b90c4a17467af13
= <sandrock11@hotmail.com>
2020-10-18 15:01:03 -0400

    added get all profiles and get profiles by id

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -5,16 +5,131 @@ import {
   PROFILE_ERROR,
 } from "./types";
 import M from "materialize-css/dist/js/materialize.min.js";
+
 import axios from "axios";
 
 //@ Action - Get Current users profile
-
 export const getCurrentProfile = () => async (dispatch) => {
   try {
     const res = await axios.get("/api/profile/me");
 
     dispatch({ type: GET_PROFILE, payload: res.data });
   } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+
+        // http status
+        status: error.response.staus,
+      },
+    });
+  }
+};
+
+
+//@Action Get all profiles
+export const getProfiles = ()=> async dispatch =>{
+
+
+  dispatch({type: CLEAR_PROFILE});
+
+  try {
+
+    const res = await axios.get('/api/profile');
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    })
+
+
+    
+  } catch (error) {
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+
+        msg: error.resonse.statusText,
+        status: error.reponse.status
+      }
+    })
+    
+  }
+}
+
+
+//@Action get user profile by userId
+
+export const getProfileById = userId => dispatch =>{
+
+  try {
+
+ 
+    const res = axios.get(`/api/profile/user/${userId}`);
+
+    dispatch({
+
+      type: GET_PROFILE,
+      payload: res.data,
+    })
+    
+  } catch (error) {
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload:{
+        
+        msg: error.response.statusText,
+        status: error.response.staus
+      }
+
+    })
+    
+  }
+
+
+}
+
+//@Action create or update profile
+/* 
+  using history object to redirect
+  using edit paramater to check if its editing or creating a profile
+*/
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    //create config object to send data
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    //make request
+
+    const res = await axios.post("/api/profile", formData, config);
+
+    //send respose to reducer
+
+    dispatch({ type: GET_PROFILE, payload: res.data });
+
+    M.toast({ html: "Profile Edited", classes: "green" });
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (error) {
+    const errors = error.response.data.errors;
+    if (errors) {
+      dispatch({ type: PROFILE_ERROR });
+      errors.forEach((error) => {
+        M.toast({ html: `${error.msg}`, classes: "red" });
+      });
+    }
+
     dispatch({
       type: PROFILE_ERROR,
       payload: {
