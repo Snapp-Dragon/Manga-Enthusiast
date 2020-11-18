7267f323092d6e546e5aa3d48dd6143546523541
= <sandrock11@hotmail.com>
2020-11-18 16:47:14 -0500

    added post reducer to root reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "../reducers/authReducer";
 import profileReducer from "../reducers/profileReducer";
+import postReducer from '../reducers/postReducer';
 
 export default combineReducers({
   authReducer,
   profileReducer,
+  postReducer
 });
