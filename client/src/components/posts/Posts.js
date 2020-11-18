3a3ac0e77e65c8ca958f6832b83b9811eccc7abc
= <sandrock11@hotmail.com>
2020-11-18 16:45:10 -0500

    added post element

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,50 @@
+import React,{Fragment,useEffect} from 'react';
+import {connect} from 'react-redux';
+import {getPosts} from '../../actions/postActions';
+import PostItem from '../posts/PostItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+
+
+const Posts = ({getPosts, posts:{posts,loading}})=>{
+
+
+    useEffect(()=>{
+
+        getPosts();
+    },[getPosts])
+
+
+    return(
+        loading ? <Spinner/> : <Fragment>
+
+        <h1>Posts</h1>
+       <p className="lead"> <i className="material-icons">group</i> Join The Fandom</p>
+       {/* Post Form */}
+
+       <div className="posts">
+           {posts.map((post)=>(
+
+               <PostItem key ={post._id} value ={post}/>
+
+           ))}
+       </div>
+
+        </Fragment>
+    )
+}
+
+Posts.propTypes = {
+
+    getPosts: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired,
+
+}
+
+const mapStateToProps = (state)=>({
+
+    posts: state.postReducer
+
+})
+
+export default connect(mapStateToProps,{getPosts}) (Posts)
