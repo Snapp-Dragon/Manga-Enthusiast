48391670fc4ae1f25d1bc34cfa51887e413b2741
= <sandrock11@hotmail.com>
2020-12-06 17:14:19 -0500

    add likes and remove likes action

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {GET_POSTS,POST_ERROR} from '../actions/types';
+import {GET_POSTS,POST_ERROR,UPDATE_LIKES} from '../actions/types';
 
 
 //@Action get posts from database
@@ -26,6 +26,80 @@ export const getPosts = ()=> async (dispatch)=>{
         dispatch({
             type: POST_ERROR,
             payload: {msg: error.response.statusText, status: error.response.status}
+        })
+        
+    }
+
+
+}
+
+//@Action update likes Add
+export const addLikes = (id)=> async (dispatch)=>{
+
+
+    try {
+
+        //add a like 
+        const res = await axios.put(`/api/post/like/${id}`);
+
+        dispatch({
+
+            type: UPDATE_LIKES,
+            payload: {
+                id,
+                likes: res.data
+            }
+         
+        })
+        
+    } catch (error) {
+
+        dispatch({
+
+            type: POST_ERROR,
+            payload: {
+
+                msg: error.response.statusText,
+                status: error.response.status
+            }
+
+
+        })
+        
+    }
+
+
+}
+
+
+//@Action update likes Remove
+
+
+export const removeLikes = (id)=> async (dispatch)=>{
+
+    try {
+        
+        const res = axios.put(`/api/post/unlike/${id}`);
+
+        dispatch({
+
+            type:  UPDATE_LIKES,
+            payload: {
+
+                id,
+                likes: res.data
+            }
+        })
+    } catch (error) {
+
+        dispatch({
+
+            type: POST_ERROR,
+            payload: {
+
+                msg: error.response.statusText,
+                status: error.response.status
+            }
         })
         
     }
