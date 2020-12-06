cc6a123f666c7bc508c341dc06c7a6ff08d11ef1
= <sandrock11@hotmail.com>
2020-12-06 17:15:51 -0500

    added on click handlers

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,49 +1,54 @@
 import React,{Fragment} from 'react'
 import PropTypes from 'prop-types'
-import Link from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import Moment from 'react-moment'
 import {connect} from 'react-redux'
+import {addLikes,removeLikes} from '../../actions/postActions';
 
 
-const PostItem = ({post:{ _id,text,name,avatar,user,likes,comments,date},auth}) => {
+const PostItem = ({auth, post:{_id,text,name,avatar,user,likes,comments,date } ,addLikes,removeLikes}) => {
     return (
         <div className="post bg-coral py-1 my-1">
         <div className = "post-image">
-          <a href="profile.html">
+          <Link  to={`/profile/${user}`}>
             <img
               className="round-img"
-              src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
+              src={avatar}
               alt=""
             />
-            <h4>John Doe</h4>
-          </a>
+            <h4>{name}</h4>
+          </Link>
         </div>
         <div>
           <p className="my-1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-            possimus corporis sunt necessitatibus! Minus nesciunt soluta
-            suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-            dolor? Illo perferendis eveniet cum cupiditate aliquam?
+            {text}
           </p>
            <p className="post-date">
-              Posted on 04/16/2019
+    Posted on <Moment format ="YYYY/MM/DD">{date}</Moment>
           </p>
-          <button type="button" className="btn btn-light">
+          <button onClick = {e => addLikes(_id)}type="button" className="btn btn-blanched">
             <i className="large material-icons">thumb_up</i>
-            <span>4</span>
+    <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
           </button>
-          <button type="button" className="btn btn-light">
+          <button onClick ={e => removeLikes(_id)} type="button" className="btn btn-blanched">
             <i className="material-icons">thumb_down</i>
           </button>
-          <a href="post.html" className="btn btn-primary">
-            Discussion <span className='comment-count'>2</span>
-          </a>
-          <button      
-          type="button"
-          className="btn btn-danger"
-        >
-          <i className="material-icons">close</i>
-        </button>
+          <Link to={`/post/${_id}`} className="btn btn-black">
+    Discussion <span className='comment-count'>{comments.length}</span>
+          </Link>
+
+          {/* Show loading button only if this is the users post */}
+          {!auth.loading && user === auth.user._id &&(
+               <button      
+               type="button"
+               className="btn btn-danger"
+             >
+               <i className="material-icons">close</i>
+             </button>
+
+
+          )}
+         
         </div>
       </div>
     )
@@ -52,13 +57,14 @@ const PostItem = ({post:{ _id,text,name,avatar,user,likes,comments,date},auth}) 
 PostItem.propTypes = {
 
     auth: PropTypes.object.isRequired,
+    post: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state=>({
  
     // we need to be able to verify who owns the post
-    post: PropTypes.object.isRequired,
     auth:  state.authReducer
 })
 
-export default connect(mapStateToProps,{}) (PostItem)
+export default connect(mapStateToProps,{addLikes,removeLikes}) (PostItem)
