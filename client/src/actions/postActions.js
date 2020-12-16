0e4a55ddbda09ddae055ef495e4587d046145da5
= <sandrock11@hotmail.com>
2020-12-15 19:58:38 -0500

    get post action

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import {GET_POSTS,POST_ERROR,UPDATE_LIKES} from '../actions/types';
+import M from "materialize-css/dist/js/materialize.min.js";
+import {GET_POSTS,GET_POST,POST_ERROR,UPDATE_LIKES,DELETE_POST,ADD_POST, ADD_COMMENT,REMOVE_COMMENT} from '../actions/types';
 
 
-//@Action get posts from database
+//@Action Get posts from database
 
 export const getPosts = ()=> async (dispatch)=>{
 
@@ -33,7 +34,43 @@ export const getPosts = ()=> async (dispatch)=>{
 
 }
 
-//@Action update likes Add
+
+//@Action get single post from the database
+
+
+export const getPost = (id)=> async (dispatch)=>{
+
+
+    try {
+
+        const res = await axios.get(`/api/post/${id}`);
+
+        dispatch({
+
+            type: GET_POST,
+            payload: res.data
+        })
+        
+    } catch (error) {
+
+        dispatch({
+
+            type: POST_ERROR,
+
+            payload: {
+
+                msg: error.response.statusText,
+                status: error.response.status
+            }
+        })
+        
+    }
+
+
+
+}
+
+//@Action Update likes Add
 export const addLikes = (id)=> async (dispatch)=>{
 
 
@@ -72,14 +109,14 @@ export const addLikes = (id)=> async (dispatch)=>{
 }
 
 
-//@Action update likes Remove
+//@Action Update likes Remove
 
 
 export const removeLikes = (id)=> async (dispatch)=>{
 
     try {
         
-        const res = axios.put(`/api/post/unlike/${id}`);
+        const res = await axios.put(`/api/post/unlike/${id}`);
 
         dispatch({
 
@@ -103,6 +140,158 @@ export const removeLikes = (id)=> async (dispatch)=>{
         })
         
     }
+
+
+}
+
+//@Action Delete Post
+
+export const deletePost = (id)=> async (dispatch)=>{
+
+
+    try {
+
+        const res = await axios.delete(`/api/post/${id}`);
+
+        dispatch({
+
+            type: DELETE_POST,
+            payload: id
+        })
+
+        M.toast({ html: "Post Deleted", classes: "green" });
+        
+    } catch (error) {
+
+        dispatch({
+
+            type: POST_ERROR,
+
+            payload: {
+
+                msg: error.response.statusText,
+                status: error.response.status
+            }
+
+        })
+        
+    }
+
+
+}
+
+//@Action add comment
+
+
+export const addComment = (postId,formData)=> async (dispatch)=>{
+
+
+    const config = {
+
+        'Content-Type': 'application/json'
+    }
+
+
+    try {
+
+
+        const res = await axios.post(`/api/post/comment/${postId}`,formData,config);
+
+
+        dispatch({
+
+            type: ADD_COMMENT,
+            payload: res.data
+        })
+
+    
+        M.toast({html: 'Comment added', classes:"green"});
+    } catch (error) {
+
+        dispatch({
+
+            type: POST_ERROR,
+            payload: {
+
+                msg: error.response.statusText,
+                status: error.resonse.status,
+            }
+        })
+        
+    }
+
+
+
+}
+
+
+//@Action delete comment
+
+export const deleteComment = (postId,commentId)=> async (dispatch)=>{
+
+
+    try {
+
+      
+        await axios.delete(`/api/post/comment/${postId}/${commentId}`);
+
+        dispatch({
+
+            type: REMOVE_COMMENT,
+            payload: commentId
+        })
+
+        M.toast({html: "Comment removed", classes: "green"});
+        
+    } catch (error) {
+
+        dispatch({
+
+            type: REMOVE_COMMENT
+        })
+        
+    }
+
+}
+
+
+export const addPost = (formData)=> async (dispatch)=>{
+
+
+    const config = {
+
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try {
+
+    
+        const res = await axios.post('/api/post',formData,config);
+
+        dispatch({
+
+            type: ADD_POST,
+            payload: res.data
+        })
+        
+        M.toast({html: "Post Created", classes: "green"});
+    } catch (error) {
+
+        dispatch({
+
+            type: POST_ERROR,
+            payload: {
+
+                msg: error.response.statusText,
+                status: error.response.status
+            }
+        })
+        
+    }
+
+
 
 
 }
